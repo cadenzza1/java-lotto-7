c082b555227519bc074176ae41e166e53763a3f9
Kim mingyu <minq0302@naver.com>
2024-11-03 03:12:36 +0900

    feat: 당첨 번호를 입력해달라는 출력문 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,7 @@
 package lotto.view;
 
 public class InputView {
+    public int inputPurchaseAmount(){
+
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public void requestPurchaseAmount(){
         System.out.println("구입금액을 입력해 주세요.");
     }
+
+    public void requestWinningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
 }
