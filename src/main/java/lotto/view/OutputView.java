9dc787a347e2c1499d7a0bfd00d8d7d04679f8fc
Kim mingyu <minq0302@naver.com>
2024-11-03 03:13:13 +0900

    feat: 보너스 번호를 입력해달라는 출력문 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public void requestWinningNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public void requestBonusNumber(){
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }
