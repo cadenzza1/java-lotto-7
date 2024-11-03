4772ab10bbb7252fb48517f64dc6d799d8324875
Kim mingyu <minq0302@naver.com>
2024-11-03 17:33:43 +0900

    refactor: 금액 입력받는 부분 수정 메서드를 분리하여 메서드들을 15줄 이내로 구현 검증을 view 측에서 하도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,20 +4,32 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public static final int THOUSAND = 1000;
+    public static final String ERROR_MESSAGE_PURCHASE_PRICE = "[ERROR] 구입 금액은 1000원 이상의 1000의 배수를 입력해주세요.";
 
     public int inputPurchasePrice(){
         while(true){
-            try{
-                int purchasePrice = Integer.parseInt(Console.readLine());
-                if(purchasePrice<THOUSAND | purchasePrice%THOUSAND != 0){
-                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 이상의 1000의 배수를 입력해주세요.");
-                }
-
+            Integer purchasePrice = validatePurchasePrice();
+            if (purchasePrice != null) {
                 return purchasePrice;
-
-            } catch(IllegalArgumentException e){
-                System.out.println("[ERROR] 구입 금액은 1000원 이상의 1000의 배수를 입력해주세요.");
             }
+        }
+    }
+
+    private static Integer validatePurchasePrice() {
+        try{
+            int purchasePrice = Integer.parseInt(Console.readLine());
+            validatePriceRange(purchasePrice);
+            return purchasePrice;
+
+        } catch(IllegalArgumentException e){
+            System.out.println(ERROR_MESSAGE_PURCHASE_PRICE);
+        }
+        return null;
+    }
+
+    private static void validatePriceRange(int purchasePrice) {
+        if(purchasePrice <THOUSAND | purchasePrice %THOUSAND != 0){
+            throw new IllegalArgumentException(ERROR_MESSAGE_PURCHASE_PRICE);
         }
     }
 }
